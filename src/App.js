75bfde6f3fbefe88d05887ef3b4e1ceaf35a5b42
John Stainsby <johnstainsby@gmail.com>
2022-05-15 13:34:35 +0100

    Setup app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppShell, Navbar, Header, Aside, Footer, Grid } from '@mantine/core';
+import GridLayout from './components/GridLayout/GridLayout';
+import styles from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <AppShell
+         padding='md'
+         navbar={
+            <Navbar width={{ base: 300 }} height={500} p='xs'>
+               {/* Navbar content */}
+            </Navbar>
+         }
+         header={<Header height={60} p='xs'></Header>}
+         styles={(theme) => ({
+            main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+         })}
+         className={styles['app']}
+      >
+         <GridLayout />
+      </AppShell>
+   );
 }
 
 export default App;
---
+++ src/components/GridLayout/GridLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Grid, Skeleton, Container } from '@mantine/core';
+
+const GridLayout = () => {
+   const child = <Skeleton height={140} radius='md' animate={false} />;
+
+   return (
+      <Container my='md'>
+         <Grid>
+            <Grid.Col xs={4}>{child}</Grid.Col>
+            <Grid.Col xs={8}>{child}</Grid.Col>
+            <Grid.Col xs={8}>{child}</Grid.Col>
+            <Grid.Col xs={4}>{child}</Grid.Col>
+            <Grid.Col xs={3}>{child}</Grid.Col>
+            <Grid.Col xs={3}>{child}</Grid.Col>
+            <Grid.Col xs={6}>{child}</Grid.Col>
+         </Grid>
+      </Container>
+   );
+};
+
+export default GridLayout;
